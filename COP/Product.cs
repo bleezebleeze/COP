5888385a2980397a416911d676ad9c789bbc6eb5
cfble <cfble@CFB32>
2025-01-30 16:03:35 -0800

    Add project files.

---
+++ COP/Product.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COP.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name {  get; set; }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            }
+        }
+        public Product()
+        {
+            Name = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spring2025.Models
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+
+        public product()
+        {
+            Name = string.Empty;
+        }
